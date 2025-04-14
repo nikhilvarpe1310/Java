47eec3d40c30282d19bb41dd0f1dfa8c139833c3
nikhilvarpe1310 <varpenikhil5179@gmail.com>
2025-04-14 15:46:57 +0530

    collection start

---
+++ eclipse/collection/collection1/src/main/java/com/demo/App.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArrayList list = new ArrayList();
+        list.add(1);
+        list.add("abc");
+        list.add(23);
+        System.out.println(list);
+    }
+}
