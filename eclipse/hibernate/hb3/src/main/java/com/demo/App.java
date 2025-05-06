6d785793d92146eabfe47ace242d16ac59a9de59
nikhilvarpe1310 <varpenikhil5179@gmail.com>
2025-05-06 21:37:59 +0530

    methods

--- eclipse/hibernate/hb3/src/main/java/com/demo/App.java
+++ eclipse/hibernate/hb3/src/main/java/com/demo/App.java
@@ -1,9 +1,10 @@
 package com.demo;
 
-import java.util.Random;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import com.model.Product;
 import com.util.HBUtils;
@@ -15,56 +16,14 @@ public class App
        Session session = HBUtils.sf.openSession();
        Transaction tx = session.beginTransaction();
        
-       String[] types = {
-                                  "Electronics",
-                                  "Clothing",
-                                  "Books",
-                                  "Home & Kitchen",
-                                  "Beauty & Personal Care",
-                                  "Sports & Outdoors",
-                                  "Toys & Games",
-                                  "Automotive",
-                                  "Health",
-                                  "Grocery",
-                                  "Furniture",
-                                  "Jewelry",
-                                  "Office Supplies",
-                                  "Pet Supplies",
-                                  "Footwear",
-                                  "Musical Instruments",
-                                  "Garden & Outdoor",
-                                  "Tools & Home Improvement",
-                                  "Baby Products",
-                                  "Industrial & Scientific",
-                                  "Mobile Phones & Accessories",
-                                  "Computer & Accessories",
-                                  "Cameras & Photography",
-                                  "Video Games",
-                                  "Watches",
-                                  "Luggage & Travel Gear",
-                                  "Art & Craft Supplies",
-                                  "Cleaning Supplies",
-                                  "Stationery",
-                                  "Lighting & Decor"
-       };
- 
-       	Random r = new Random(); 
-       	for(int i=1; i<=10000; i++)
-       	{
-       		Product p = new Product();
-       		StringBuilder sb = new StringBuilder();
-       		for(int j=1; j<=15; j++)
-       			sb.append((char)(r.nextInt(26)+65));
-       		p.setName(sb.toString());
-       		p.setType(types[r.nextInt(types.length)]);
-       		p.setPrice(r.nextDouble()*1000000);
-       		session.save(p);
-       	}
-      
+     Product p = session.createQuery("from Product where id=:a",Product.class)
+       .setParameter("a", 50)
+       .uniqueResult();
+       System.out.println(p);
        
        tx.commit();
        
        
-       System.out.println();
+      
     }
 }
